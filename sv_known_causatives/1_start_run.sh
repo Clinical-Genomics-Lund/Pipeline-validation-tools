b8be976a8244eeef24a105aa084fe72c0213ee75
Jakob Willforss <jakob.willforss@hotmail.com>
2024-04-02 14:33:25 +0200

    Evaluation tools for SV

---
+++ sv_known_causatives/1_start_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <csvs dir>"
+    exit 1
+fi
+
+input_csvs=$1
+
+nbr_csvs=$(ls ${input_csvs}/*.csv | wc -l)
+
+echo "Found ${nbr_csvs} csvs"
+echo "${input_csvs}"
+read -p "Do you want to start the runs? (y/n) " -n 1 -r
+echo
+if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+    for csv in ${input_csvs}/*.csv; do
+        /fs2/sw/bnf-scripts/start_nextflow_analysis.pl ${csv}
+    done
+fi
