2ec0ee44dc4c8eb692c50a09ea1ab856a7009be2
Jakob Willforss <jakob.willforss@hotmail.com>
2024-04-04 17:55:12 +0200

    Prepare evaluate_run.py

--- sv_known_causatives/scripts/evaluate_run.py
+++ sv_known_causatives/scripts/evaluate_run.py
@@ -4,6 +4,7 @@ import argparse
 import subprocess
 import pathlib
 import shutil
+import io
 
 
 def main():
@@ -29,21 +30,29 @@ def main():
                 first_line = False
                 continue
         
-        line = line.rstrip()
-        fields = line.split(",")
-        label = fields[0]
-        result = fields[1]
-        baseline = fields[2]
+            line = line.rstrip()
+            fields = line.split(",")
+            label = fields[0]
+            result_vcf = fields[1]
+            baseline_vcf = fields[2]
 
-        print(f"Looking for matches in {label}")
-        out_fp = f"{args.outdir}/{label}.query_out.vcf"
+            print(f"Looking for matches in {label}")
+            out_fp = f"{args.outdir}/{label}.query_out.vcf"
+            out_baseline_fp = f"{out_fp}.baseline"
+            print(f"Writing baseline to {out_baseline_fp}")
+            write_baseline(baseline_vcf, out_baseline_fp)
 
-        write_baseline()
+            match_fp = f"{out_baseline_fp}.match"
+            match_path = pathlib.Path(match_fp)
+            if match_path.exists():
+                print(f"{match_path} already exists, skipping (FIXME: Force flag)")
+            else:
+                run_svdb(args.bnd_distance, args.overlap, baseline_vcf, result_vcf, match_fp)
 
         # Check if .match exists
         # If so, skip
         # else
-        run_svdb(args.bnd_distance, args.overlap, baseline, result)
+        # run_svdb(args.bnd_distance, args.overlap, baseline_vcf, result_vcf)
 
 
     print_summary()
@@ -53,12 +62,52 @@ def check_svdb() -> bool:
     return shutil.which(command) is not None
 
 
-def write_baseline():
-    pass
+def write_baseline(baseline_vcf_fp: str, out_fp: str):
+    with open (baseline_vcf_fp) as in_fh, open(out_fp, 'w') as out_fh:
+        for line in in_fh:
+            line = line.rstrip()
+            if not line.startswith("#"):
+                print(line, file=out_fh)
 
 
-def run_svdb(bnd_distance: int, overlap: float, baseline: str, query_vcf: str):
-    pass
+
+def run_svdb(bnd_distance: int, overlap: float, baseline: str, query_vcf: str, out_fp: str):
+
+    print(query_vcf)
+
+    command = [
+        "svdb",
+        "--query",
+        "--bnd_distance",
+        f"{bnd_distance}",
+        "--overlap",
+        f"{overlap}",
+        "--db",
+        f"{baseline}",
+        "--query_vcf",
+        f"{query_vcf}",
+        "--out_occ",
+        "MATCH"
+    ]
+
+    print(" ".join(command))
+
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+
+    # FIXME: Read the STDOUT
+    # singularity run -B /fs1 <svdb container> python3 Pipeline-validation-tools/sv_known_causatives/scripts/evaluate_run.py --csv eval_spreadsheet_test.csv --outdir testout
+
+    # print(result.stdout)
+    # while True:
+    #     line = proc.stdout.readline()
+    #     if not line:
+    #         break
+    #     if not line.startswith("#") and line.find("MATCH") != -1:
+    #         print(line)
+
+    raise ValueError("stop")
+
+
     # svdb \
     # --query \
     # --bnd_distance ${bnd_distance} \
